737e819445a242a686944c2d63cfccaeb730dad4
samuel-garcia <samuel.garcia@tokopedia.com>
2019-08-12 13:57:49 +0700

    test

---
+++ Object Repository/objek.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>objek</name>
+   <tag></tag>
+   <elementGuidId>61329d4c-4bd0-451d-a229-9abb7e40fcca</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
